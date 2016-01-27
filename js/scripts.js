b82d990a2d56e6843c2ba17d090a7864b254daf4
Jason Awbrey and Sara Wagner <sara.wagner23q@gmail.com>
2016-01-27 11:18:04 -0800

    Add beginning functionality to Hangman constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-// Uncomment this to see the example spec test pass. Delete if you don't need it!
-// var helloWorld = function(){
-//   return false;
-// };
+var Hangman = function(){
+  this.remainingLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  this.listOfWords = ["Apple", "Banana", "Water Melon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato"];
+  this.wordToGuess = "";
+  this.usedLetters = [];
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,9 @@
-// Example spec. Uncomment if you want to see it run in spec-runner.html! Then uncomment the function in scripts.js
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+describe("Hangman", function(){
+  it("Will create a new game of hangman", function() {
+    var currentGame = new Hangman();
+    expect(currentGame.remainingLetters).to.eql(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]);
+    expect(currentGame.listOfWords).to.eql(["Apple", "Banana", "Water Melon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato"]);
+    expect(currentGame.wordToGuess).to.equal("");
+    expect(currentGame.usedLetters).to.eql([]);
+  });
+});
