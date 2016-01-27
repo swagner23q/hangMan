53ee953d83d3d138c700852d10320b67a9f7b3e4
Jason Awbrey and Sara Wagner <jason.s.awbrey@gmail.com>
2016-01-27 13:40:48 -0800

    Add blanks will fill for userGuess

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var Hangman = function(){
   this.remainingLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  this.listOfWords = ["Apple", "Banana", "Water Melon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato", "Date"];
+  this.listOfWords = ["Apple", "Banana", "Watermelon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato", "Date"];
   this.wordToGuess = "";
   this.usedLetters = [];
   this.userGuess = [];
@@ -10,27 +10,29 @@ var Hangman = function(){
 Hangman.prototype.wordChooser = function() {
   var chooseRandomNumber = Math.random();
   var wordChoice = "";
-  if (chooseRandomNumber > 0) {
-    return true;
-  } else if (chooseRandomNumber > .1) {
-     return true;
-  } else if (chooseRandomNumber > .2) {
-     return true;
-  } else if (chooseRandomNumber > .3) {
-     return true;
-  } else if (chooseRandomNumber > .4) {
-     return true;
-  } else if (chooseRandomNumber > .5) {
-     return true;
-  } else if (chooseRandomNumber > .6) {
-     return true;
-  } else if (chooseRandomNumber > .7) {
-     return true;
-  } else if (chooseRandomNumber > .8) {
-     return true;
-  } else if (chooseRandomNumber > .9) {
-     return true;
-  } else {
-     return false;
-  }
-}
+  if (chooseRandomNumber > .9) {
+     wordChoice = currentGame.listOfWords[9];
+   } else if (chooseRandomNumber > .8) {
+      wordChoice = currentGame.listOfWords[8];
+   } else if (chooseRandomNumber > .7) {
+       wordChoice = currentGame.listOfWords[7];
+   } else if (chooseRandomNumber > .6) {
+       wordChoice = currentGame.listOfWords[6];
+   } else if (chooseRandomNumber > .5) {
+        wordChoice = currentGame.listOfWords[5];
+   } else if (chooseRandomNumber > .4) {
+        wordChoice = currentGame.listOfWords[4];
+   } else if (chooseRandomNumber > .3) {
+        wordChoice = currentGame.listOfWords[3];
+   } else if (chooseRandomNumber > .2) {
+        wordChoice = currentGame.listOfWords[2];
+   } else if (chooseRandomNumber > .1) {
+        wordChoice = currentGame.listOfWords[1];
+   } else if (chooseRandomNumber < .1) {
+       wordChoice = currentGame.listOfWords[0];
+     }
+     currentGame.wordToGuess = wordChoice.split("");
+     for(i = 0; i < currentGame.wordToGuess.length; i++){
+       currentGame.userGuess.push("_");
+     }
+   }
--- spec/specs.js
+++ spec/specs.js
@@ -2,16 +2,19 @@ describe("Hangman", function(){
   it("Will create a new game of hangman", function() {
     var currentGame = new Hangman();
     expect(currentGame.remainingLetters).to.eql(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]);
-    expect(currentGame.listOfWords).to.eql(["Apple", "Banana", "Water Melon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato"]);
+    expect(currentGame.listOfWords).to.eql(["Apple", "Banana", "Watermelon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato", "Date"]);
     expect(currentGame.wordToGuess).to.equal("");
     expect(currentGame.usedLetters).to.eql([]);
     expect(currentGame.userGuess).to.eql([]);
     expect(currentGame.wrongAnswerCount).to.equal(0);
   });
 });
+
 describe("wordChooser", function() {
-  it("Will choose a word from a list at random", function() {
+    it("Will choose a word from a list at random", function() {
     var currentGame = new Hangman();
     expect(currentGame.wordChooser()).to.equal(true);
   });
+
+  //Spec should be written for adding the correct amount of blanks to
 });
