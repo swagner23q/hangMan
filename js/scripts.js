20abc54d924fb9b31f33aadf957acf0da112f4fd
Jason Awbrey and Sara Wagner <jason.s.awbrey@gmail.com>
2016-01-27 16:35:58 -0800

    Add most functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,65 @@
 var Hangman = function(){
-  this.remainingLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  this.listOfWords = ["Apple", "Banana", "Watermelon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato", "Date"];
+  this.availableLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  this.listOfWords = ["apple", "banana", "watermelon", "orange", "pear", "grapes", "strawberries", "kiwi", "tomato", "date"];
   this.wordToGuess = "";
-  this.usedLetters = [];
+  this.guessedLetters = [];
   this.userGuess = [];
+  this.currentLetter = "";
   this.wrongAnswerCount = 0;
 };
 
 Hangman.prototype.wordChooser = function() {
   var word = this.listOfWords[Math.floor(Math.random() * this.listOfWords.length)];
-  return this.wordToGuess = word;
+  return this.wordToGuess = word.split("");
 }
 
 Hangman.prototype.blankCreator = function () {
   for (var i = 0; i < this.wordToGuess.length; i++) {
     this.userGuess.push("_");
   } return this.userGuess;
+};
+
+
+
+
+
+Hangman.prototype.chooseNewLetter = function (currentLetter) {
+  if(this.checkLetter(currentLetter)){
+    this.replaceBlanks(currentLetter);
+  } else {
+    this.increaseWrongAnswerCount();
+  }
+   this.moveToGuessed(currentLetter);
+
+   if(this.wrongAnswerCount === 7){
+     alert("You Lost!");
+   } else if (this.userGuess.join("") === this.wordToGuess.join("")){
+     alert("You won!");
+   }
+};
+
+Hangman.prototype.checkLetter = function (currentLetter) {
+  if(this.wordToGuess.indexOf(currentLetter) !== -1){
+    return true;
+  } return false;
+};
+
+Hangman.prototype.replaceBlanks = function (currentLetter) {
+  for (var i = 0; i < this.wordToGuess.length; i++) {
+    if(this.wordToGuess[i] === currentLetter){
+      this.userGuess[i] = currentLetter
+    }
+  }  return this.userGuess;
+};
+
+Hangman.prototype.increaseWrongAnswerCount = function () {
+  this.wrongAnswerCount++;
+  return this.wrongAnswerCount;
+};
+
+Hangman.prototype.moveToGuessed = function (currentLetter) {
+  var i = this.availableLetters.indexOf(currentLetter);
+  this.availableLetters.splice(i,1);
+  this.guessedLetters.push(currentLetter);
+  return this.guessedLetters;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,38 +1,65 @@
 describe("Hangman", function(){
   it("Will create a new game of hangman", function() {
     var currentGame = new Hangman();
-    expect(currentGame.remainingLetters).to.eql(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]);
-    expect(currentGame.listOfWords).to.eql(["Apple", "Banana", "Watermelon", "Orange", "Pear", "Grapes", "Strawberries", "Kiwi", "Tomato", "Date"]);
+    expect(currentGame.availableLetters).to.eql(["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]);
+    expect(currentGame.listOfWords).to.eql(["apple", "banana", "watermelon", "orange", "pear", "grapes", "strawberries", "kiwi", "tomato", "date"]);
     expect(currentGame.wordToGuess).to.equal("");
-    expect(currentGame.usedLetters).to.eql([]);
+    expect(currentGame.guessedLetters).to.eql([]);
     expect(currentGame.userGuess).to.eql([]);
+    expect(currentGame.currentLetter).to.equal("");
     expect(currentGame.wrongAnswerCount).to.equal(0);
   });
 });
 
 describe("wordChooser", function() {
-    it("Will choose a word from a list at random, and pass when random word matches equal value and fail if not matched (the code functions properly with random aspect)", function() {
+    it("Will choose a word from listOfWords at RANDOM, and pass when RANDOM word matches equal value (set to Apple now) and fail if not matched (the code functions properly with RANDOM aspect)", function() {
     var currentGame = new Hangman();
-    expect(currentGame.wordChooser()).to.equal("Apple");
+    expect(currentGame.wordChooser()).to.equal("apple");
   });
-  //Spec should be written for adding the correct amount of blanks to
 });
 
 describe("blankCreator", function(){
-  it("Will push enough blanks into the userGuess property equal to the length of the wordToGuess, and pass when random word matches equal value and fail if not matched (the code functions properly with random aspect)", function(){
+  it("Will push enough blanks into the userGuess property equal to the length of the wordToGuess, and pass when RANDOM word matches equal value (set to five blank spaces for Apple now) and fail if not matched (the code functions properly with RANDOM aspect)", function(){
     var currentGame = new Hangman();
     currentGame.wordChooser();
     expect(currentGame.blankCreator()).to.eql(["_","_","_","_","_"]);
   });
 });
 
+describe("checkLetter", function(){
+  it("Will check the user letter choice against the wordToGuess", function(){
+    var currentGame = new Hangman();
+    currentGame.wordToGuess = "apple";
+    expect(currentGame.checkLetter("a")).to.equal(true);
+  });
+});
 
+describe("replaceBlanks", function(){
+  it("Will replace blanks with letter IF the letter is found in the wordToGuess", function(){
+    var currentGame = new Hangman();
+    currentGame.wordToGuess = "apple";
+    currentGame.currentLetter = "a";
+    currentGame.userGuess = ["_","_","_","_","_"];
+    expect(currentGame.replaceBlanks("a")).to.eql(["a","_","_","_","_"]);
+  });
+});
 
-// describe("drawBodyPart", function() {
-//   it("Will set the correct body part image according to the wrongAnswerCount", function() {
-//     var currentGame = new Hangman();
-//     currentGame.wrongAnswerCount = 1;
-//     expect(drawBodyPart
-//
-//     )};
-// });
+describe("moveToGuessed", function(){
+  it("Will move a letter that was guessed to the guessedLetters array", function(){
+    var currentGame = new Hangman();
+    expect(currentGame.moveToGuessed("a")).to.eql(["a"]);
+  });
+});
+
+describe("increaseWrongAnswerCount", function(){
+  it("Will increase the wrong answer count by 1", function(){
+    var currentGame = new Hangman();
+    expect(currentGame.increaseWrongAnswerCount()).to.equal(1);
+  })
+})
+
+// describe("chooseNewLetter", function(){
+//   it("Will run through all the methods to see what needs to happen", function(){
+//     expect(shit to happen)
+//   })
+// })
