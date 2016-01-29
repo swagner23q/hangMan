0b9361de476f0770b4be89794260a76f580c8091
Jason Awbrey and Sara Wagner <sara.wagner23q@gmail.com>
2016-01-28 16:02:36 -0800

    so many bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
 $(function(){
+  var currentGame = new Hangman();
+  // currentGame.wordChooser();
+  // currentGame.blankCreator();
+  // currentGame.displayAvailableLetters();
+  // currentGame.displayUserGuess();
+
   $('#newGameButton').click(function(){
     $('#platformSpace').empty();
     $('#platformSpace').append("<img src='images/hangman0.png'>");
-    var currentGame = new Hangman();
+    $('#guessedLetterSpace').find('ul').empty();
+    $('#availableLetterSpace').find('ul').empty();
+    $('#userGuessSpace').find('ul').empty();
+    currentGame.availableLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     currentGame.wordChooser();
     currentGame.blankCreator();
+    currentGame.guessedLetters = [];
+    currentGame.wrongAnswerCount= 0;
     currentGame.displayAvailableLetters();
     currentGame.displayUserGuess();
-
     $('#availableLetterSpace').on('click', 'li', function(){
-      var clickedLetter = $(this).text();
-      currentGame.runHangman(clickedLetter);
+      var selectedLetter = $(this).text();
+      currentGame.runHangman(selectedLetter);
     });
   });
 
@@ -26,30 +36,32 @@ var Hangman = function(){
 };
 
 
-
-Hangman.prototype.logSomething = function () {
-  $('#guessWordButton').click(function(){
-    console.log("logging something");
-  });
-};
-
-
-
-
-
-
 Hangman.prototype.wordChooser = function() {
   var word = this.listOfWords[Math.floor(Math.random() * this.listOfWords.length)];
   return this.wordToGuess = word.split("");
 }
 
 Hangman.prototype.blankCreator = function () {
+  this.userGuess = [];
   for (var i = 0; i < this.wordToGuess.length; i++) {
     this.userGuess.push("_");
   } return this.userGuess;
 };
 
+Hangman.prototype.displayAvailableLetters = function () {
+  $('#availableLetterSpace').find("ul").empty();
+  this.availableLetters.forEach(function(i){$("#availableLetterSpace").find("ul").append("<li>" +i + "</li>")});
+};
 
+Hangman.prototype.displayGuessedLetters = function () {
+  $('#guessedLetterSpace').find("ul").empty();
+  this.guessedLetters.forEach(function(i){$("#guessedLetterSpace").find("ul").append("<li>" +i + "</li>")});
+};
+
+Hangman.prototype.displayUserGuess = function () {
+  $('#userGuessSpace').find("ul").empty();
+  this.userGuess.forEach(function(i){$("#userGuessSpace").find("ul").append("<li>" +i + "</li>")});
+};
 
 Hangman.prototype.runHangman = function (currentLetter) {
   if(this.checkLetter(currentLetter)){
@@ -63,11 +75,16 @@ Hangman.prototype.runHangman = function (currentLetter) {
    this.displayAvailableLetters();
    this.displayGuessedLetters();
    this.displayUserGuess();
+   this.checkOutcome();
+};
 
-   if(this.wrongAnswerCount === 6){
-   } else if (this.userGuess.join("") === this.wordToGuess.join("")){
-     alert("You won!");
-   }
+
+Hangman.prototype.checkOutcome = function () {
+  if(this.wrongAnswerCount === 6){
+    $('#outComeSpace').text("You Lost!");
+  } else if (this.userGuess.join("") === this.wordToGuess.join("")){
+    $('#outComeSpace').text("You Won!");
+  }
 };
 
 Hangman.prototype.checkLetter = function (currentLetter) {
@@ -79,7 +96,7 @@ Hangman.prototype.checkLetter = function (currentLetter) {
 Hangman.prototype.replaceBlanks = function (currentLetter) {
   for (var i = 0; i < this.wordToGuess.length; i++) {
     if(this.wordToGuess[i] === currentLetter){
-      this.userGuess[i] = currentLetter
+      this.userGuess[i] = currentLetter;
     }
   }  return this.userGuess;
 };
@@ -94,19 +111,4 @@ Hangman.prototype.moveToGuessed = function (currentLetter) {
   this.availableLetters.splice(i,1);
   this.guessedLetters.push(currentLetter);
   return this.guessedLetters;
-};
-
-Hangman.prototype.displayAvailableLetters = function () {
-  $('#availableLetterSpace').first("ul").children().remove();
-  this.availableLetters.forEach(function(i){$("#availableLetterSpace").first("ul").append("<li>" +i + "</li>")});
-};
-
-Hangman.prototype.displayGuessedLetters = function () {
-  $('#guessedLetterSpace').first("ul").children().remove();
-  this.guessedLetters.forEach(function(i){$("#guessedLetterSpace").first("ul").append("<li>" +i + "</li>")});
-};
-
-Hangman.prototype.displayUserGuess = function () {
-  $('#userGuessSpace').first("ul").children().remove();
-  this.userGuess.forEach(function(i){$("#userGuessSpace").first("ul").append("<li>" +i + "</li>")});
 };
